5414380282b44e59fffa4782608a469e49732e44
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2024-06-12 14:51:54 +0100

    Creating Diagnosis Views

---
+++ Controllers/DiagnosisController.cs
@@ -0,0 +1,30 @@
+using Lealthy_Hospital_Application_System.Models;
+using Lealthy_Hospital_Application_System.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lealthy_Hospital_Application_System.Controllers;
+
+public class DiagnosisController : Controller
+{
+    private readonly IDiagnosisRepository _diagnosisRepository;
+    private readonly IStaffRepository _staffRepository;
+    private readonly IPatientRepository _patientRepository;
+
+    public DiagnosisController(IDiagnosisRepository _diagnosisRepository)
+    {
+        this._diagnosisRepository = _diagnosisRepository;
+    }
+    
+    public IActionResult Index()
+    {
+        List<DiagnosisModel> ListAll = _diagnosisRepository.GetAllDiagnosis();
+        return View(ListAll);
+    }
+
+    public IActionResult CreateDiagnosis()
+    {
+        return View();
+    }
+    
+    
+}
