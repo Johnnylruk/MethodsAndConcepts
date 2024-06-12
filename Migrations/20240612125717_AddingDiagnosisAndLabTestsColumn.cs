6ffa21de4d6c9bae947d9bfcf96b5ad8ad43ad78
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2024-06-12 13:58:36 +0100

    Adding Column to Diagnosis and LabTest Database

---
+++ Migrations/20240612125717_AddingDiagnosisAndLabTestsColumn.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LealthyHospitalApplicationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDiagnosisAndLabTestsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PatientName",
+                table: "LabTests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StaffName",
+                table: "LabTests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PatientName",
+                table: "Diagnosis",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StaffName",
+                table: "Diagnosis",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientName",
+                table: "LabTests");
+
+            migrationBuilder.DropColumn(
+                name: "StaffName",
+                table: "LabTests");
+
+            migrationBuilder.DropColumn(
+                name: "PatientName",
+                table: "Diagnosis");
+
+            migrationBuilder.DropColumn(
+                name: "StaffName",
+                table: "Diagnosis");
+        }
+    }
+}
