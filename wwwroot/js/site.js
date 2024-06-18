4c72f508d6a27416e0fd16a138f1d0613a23be2b
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2024-06-18 15:17:21 +0100

    Adding DataTable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,33 @@ $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
 
+/*DataTable*/
+$(document).ready(function () {
+    // Find the index of the column with the empty header
+    let emptyHeaderIndex = [];
+    $('#tables thead th').each(function (index) {
+        if ($(this).text().trim() === '') {
+            emptyHeaderIndex.push(index);
+        }
+    });
+
+    // Initialize DataTable with columnDefs
+    $('#tables').DataTable({
+        "columnDefs": [
+            {
+                "targets": "_all", // Target all columns
+                "className": "text-center" // Apply 'text-center' class to center align content
+            },
+            {
+                "targets": emptyHeaderIndex, // Target columns with empty header
+                "searchable": false // Make these columns not searchable
+            }
+        ]
+    });
+});
+
+/*DatePicker*/
+
 document.addEventListener("DOMContentLoaded", function() {
     var dateTimePicker = flatpickr("#datetimepicker", {
         enableTime: true,
